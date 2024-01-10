9cc3d5074b4a3fa665b0f78d410d10b70ff86351
Cynthia Delevante <cynthiadelevante@Cynthias-MacBook-Air.local>
2024-01-09 19:35:33 -0500

    4-4 update

--- js/index.js
+++ js/index.js
@@ -22,3 +22,36 @@ for(let i = 0 ; i < skills.length; i++) {
     skill.innerHTML = skills[i] 
     skillsList.appendChild(skill)
 }
+
+let messageForm = document.querySelector('form[name = "leave_message"]'); 
+messageForm.addEventListener("submit", (event) => { 
+    event.preventDefault();
+let usersName= event.target.usersName.value
+let usersEmail= event.target.usersEmail.value
+let usersMessage = event.target.usersMessage.value
+console.log("hello", usersName, usersEmail, usersMessage)
+
+}); 
+
+let messageSection = document.getElementById ('messages');
+let messageList = messageSection.querySelector('ul');
+let newMessage = document.createElement('li') ;
+
+newMessage.innerHTML=  
+`
+    <a href="mailto:${usersEmail}">${usersName}</a>
+    <span>${usersMessage}</span>
+  `;
+
+  const removeButton = document.createElement('button'); 
+  removeButton.innerText = 'Remove' ;
+  removeButton.type = 'button'; 
+
+  removeButton.addEventListener('click', function(event) {
+    const entry = event.target.parentNode;
+    entry.remove();
+  }); 
+
+  messageList.append(newMessage);
+  newMessage.appendChild(removeButton) ;
+  event.target.reset ();
